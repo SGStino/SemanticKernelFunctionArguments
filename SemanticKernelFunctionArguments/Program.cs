34738c73d6f9eb86218a7011fd27b6e2c8709eb1
Stijn TALLON <Stijn.TALLON@vinci-energies.net>
2025-06-30 09:34:34 +0200

    Add project files.

---
+++ SemanticKernelFunctionArguments/Program.cs
@@ -0,0 +1,91 @@
+﻿
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+
+
+async Task RunTest(KernelFunction[] functions, Dictionary<string, List<string>> todos)
+{
+    var kernelBuilder = Kernel.CreateBuilder()
+        .AddAzureOpenAIChatCompletion(config["deployment"], config["endpoint"], config["apiKey"], modelId: config["modelId"]);
+    kernelBuilder.Plugins.AddFromFunctions("todos", functions);
+
+
+    var kernel = kernelBuilder.Build();
+
+
+    var completionService = kernel.GetRequiredService<IChatCompletionService>();
+
+    var history = new ChatHistory();
+    history.AddSystemMessage("You are an assistant that can manage use TODOs");
+    history.AddUserMessage("Can you add 5 common items to my shopping todo list? Make sure you don't have duplicates, if there are any, tell me what they are.");
+    var completion = await completionService.GetChatMessageContentsAsync(history, new()
+    {
+        FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+    }, kernel);
+
+    Console.WriteLine("--");
+    foreach (var message in completion)
+        Console.WriteLine(message.Content);
+
+    foreach (var list in todos)
+    {
+        Console.WriteLine(list.Key);
+        foreach (var item in list.Value)
+        {
+            Console.WriteLine($"  - {item}");
+        }
+    }
+}
+
+var todos1 = new TodoState();
+var todos2 = new TodoState();
+
+await RunTest([
+        KernelFunctionFactory.CreateFromMethod(todos1.add_todos, new(){
+            FunctionName = "add_todos",
+            Description = "Add todo items to a specific scope/group"
+        }),
+        KernelFunctionFactory.CreateFromMethod(todos1.remove_todos, new(){
+            FunctionName = "remove_todos",
+            Description = "Remove todo items from a specific scope/group"
+        }),
+        KernelFunctionFactory.CreateFromMethod(todos1.get_todos, new(){
+            FunctionName = "get_todos",
+            Description = "List all todo items of a specific scope/group"
+        }),
+        KernelFunctionFactory.CreateFromMethod(todos1.get_scopes, new(){
+            FunctionName = "get_Scopes",
+            Description = "List all known scopes/groups"
+        })
+    ], todos1.todos);
+
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+await RunTest([
+        AIFunctionFactory.Create(todos2.add_todos, new(){
+            Name = "add_todos",
+            Description = "Add todo items to a specific scope/group"
+        }).AsKernelFunction(),
+        AIFunctionFactory.Create(todos2.remove_todos, new(){
+            Name = "remove_todos",
+            Description = "Remove todo items from a specific scope/group"
+        }).AsKernelFunction(),
+        AIFunctionFactory.Create(todos2.get_todos, new(){
+            Name = "get_todos",
+            Description = "List all todo items of a specific scope/group"
+        }).AsKernelFunction(),
+        AIFunctionFactory.Create(todos2.get_scopes, new(){
+            Name = "get_Scopes",
+            Description = "List all known scopes/groups"
+        }).AsKernelFunction()
+    ], todos2.todos);
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
---
+++ SemanticKernelFunctionArguments/TodoState.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SemanticKernel;
+
+public class TodoState
+{
+
+    public Dictionary<string, List<string>> todos = new Dictionary<string, List<string>>()
+    {
+        ["shopping"] = ["bread", "milk", "eggs", "bananas"]
+    };
+    public string[] add_todos(string[] items, string scope)
+    {
+        if (!todos.TryGetValue(scope, out var result))
+            todos[scope] = result = [];
+        Console.WriteLine($"@add_todos({string.Join(", ", items)}, {scope})");
+        result.AddRange(items.Except(result));
+        return [.. result];
+    }
+    public int remove_todos(string[] items, string scope)
+    {
+        if (todos.TryGetValue(scope, out var result))
+            return result.RemoveAll(items.Contains);
+        Console.WriteLine($"@remove_todos({string.Join(", ", items)}, {scope})");
+        return 0;
+    }
+
+    public string[] get_todos(string scope)
+    {
+        Console.WriteLine($"@get_todos({scope})");
+        if (todos.TryGetValue(scope, out var result))
+            return [.. result];
+        return [];
+    }
+
+    public string[] get_scopes()
+    {
+        Console.WriteLine($"@get_scopes()");
+        return [.. todos.Keys];
+    }
+
+}
